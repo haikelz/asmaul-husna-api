2b1053be8ea394f5e55c791b27c057e35088cbd9
haikelz <haikel.2017th@gmail.com>
2023-07-25 17:23:07 +0700

    fix: `app.controller` testing

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -15,8 +15,16 @@ describe("AppController", () => {
   });
 
   describe("root", () => {
-    it("should return array of object", () => {
-      expect(appController.getHomeData).toBe({});
+    it("should return an object", () => {
+      expect(appController.getHomeData()).toStrictEqual({
+        author: "Haikel Ilham Hakim",
+        repository: "https://github.com/haikelz/asmaul-husna-api",
+        endpoints: {
+          "/api/all": "Get all Asma'ul Husna",
+          "/api/:urutan": "Get spesific Asma'ul Husna based on urutan",
+          "/api/latin/:latin": "Get spesific Asma'ul Husna based on latin",
+        },
+      });
     });
   });
 });
